dbaadadbfe0373f5d800b649f2fe3cb61381cbd0
Simon LETORT <simon@artwaresolutions.org>
2011-05-22 21:46:48 -0400

    ok

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,5 @@
 class Artwork < ActiveRecord::Base
   require 'carrierwave'
-  include ImageUploader::CarrierWave
   attr_accessible :artist, :title, :image, :year, :description, :medium, :dimensions, :exhibition_ids
   has_many :exhibitions_artworks
   has_many :exhibitions, :through => :exhibitions_artworks
