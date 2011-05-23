4d4826838a9aca5cfdee67ac0a35a0bbb129536a
Simon LETORT <simon@artwaresolutions.org>
2011-05-22 21:25:52 -0400

    require carrierwave in rb file

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,5 @@
 class Artwork < ActiveRecord::Base
+  require 'carrierwave'
   attr_accessible :artist, :title, :image, :year, :description, :medium, :dimensions, :exhibition_ids
   has_many :exhibitions_artworks
   has_many :exhibitions, :through => :exhibitions_artworks
