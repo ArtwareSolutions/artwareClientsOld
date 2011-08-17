ac86067dfc0a3386806b9172ea09b80ca69b468f
Simon LETORT <simon@artwaresolutions.org>
2011-08-16 22:52:17 -0300

    removed fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,20 +9,22 @@ CarrierWave.configure do |config|
   #   config.root = File.join(Rails.root, 'public')
   # 
   # when :production
-    # config.storage = :s3
-    # config.s3_access_key_id = ENV['S3_KEY_ID']
-    # config.s3_secret_access_key = ENV['S3_SECRET_KEY']
-    # config.s3_bucket = ENV['S3_BUCKET']
+        
+    #config.fog_credentials = {
+    #  :aws_access_key_id => ENV['S3_KEY_ID'],
+    #  :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    #  :provider => 'AWS'
+    #}
+    #config.fog_directory = ENV['S3_BUCKET']
+    #config.fog_public = true
     
+    config.storage = :s3
+    config.s3_access_key_id = ENV['S3_KEY_ID']
+    config.s3_secret_access_key = ENV['S3_SECRET_KEY']
+    config.s3_bucket = ENV['S3_BUCKET']
     
-    config.fog_credentials = {
-      :aws_access_key_id => ENV['S3_KEY_ID'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :provider => 'AWS'
-    }
-    config.fog_directory = ENV['S3_BUCKET']
-    config.fog_public = true
+    config.s3_access_policy = :public_read
     
-  end
+end
   
 # end
